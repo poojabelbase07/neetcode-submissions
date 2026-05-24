a593a8d8b4d3147388199fd0f7806abd94500172
Pooja Belbase <115167026+poojabelbase07@users.noreply.github.com>
2026-05-24 20:58:03 +0530

    Add: subsets - submission-2

---
+++ Data Structures & Algorithms/subsets/submission-2.java
@@ -0,0 +1,33 @@
+
+class Solution {
+
+    public List<List<Integer>> subsets(int[] nums) {
+
+        List<List<Integer>> result = new ArrayList<>();
+
+        backtrack(0, nums, new ArrayList<>(), result);
+
+        return result;
+    }
+
+    private void backtrack(int index,
+                           int[] nums,
+                           List<Integer> current,
+                           List<List<Integer>> result) {
+
+        // add current subset
+        result.add(new ArrayList<>(current));
+
+        for (int i = index; i < nums.length; i++) {
+
+            // choose
+            current.add(nums[i]);
+
+            // explore
+            backtrack(i + 1, nums, current, result);
+
+            // un-choose (backtrack)
+            current.remove(current.size() - 1);
+        }
+    }
+}
